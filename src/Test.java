a61784ceaecd76b863ef17375982c761c17e7381
handsomepotato <806905452@qq.com>
2017-09-23 16:34:11 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        if (args == null) {
+
+        } else {
+            if (args == null) {
+                
+            }
+        }
+    }
+}
